d29d0997569c7bfc0db6d5abff5d75b3058bc112
Diego Francisco <diegofrancisco.ks@gmail.com>
2018-02-18 13:39:21 -0300

    Implementing data load task.

--- PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/MoviePostersActivity.java
+++ PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/MoviePostersActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import com.nanodegree.diego.popularmovies.adapter.MoviePostersListAdapter;
+import com.nanodegree.diego.popularmovies.keys.ConstantKeys;
 import com.nanodegree.diego.popularmovies.tasks.LoadMoviesTask;
 
 public class MoviePostersActivity extends AppCompatActivity {
@@ -16,6 +17,12 @@ public class MoviePostersActivity extends AppCompatActivity {
      * The max number of posters shown into the list.
      */
     public final static int MAX_MOVIE_POSTERS_COUNT = 20;
+
+    /**
+     * Request url for the Movie db API.
+     */
+    public final static String MOVIE_DB_MOST_POPULAR_URL_REQUEST = String.format("https://api.themoviedb.org/3/movie/popular?api_key={0}",
+            ConstantKeys.MOVIE_DB_API_KEY);
 
     /**
      * RecyclerView reference.
@@ -77,6 +84,6 @@ public class MoviePostersActivity extends AppCompatActivity {
 
         // TODO create a method to load the query preferences (order, popularity, etc...)
 
-        new LoadMoviesTask(this).execute();
+        new LoadMoviesTask(this).execute(MOVIE_DB_MOST_POPULAR_URL_REQUEST);
     }
 }
--- PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/tasks/LoadMoviesTask.java
+++ PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/tasks/LoadMoviesTask.java
@@ -1,13 +1,26 @@
 package com.nanodegree.diego.popularmovies.tasks;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.nanodegree.diego.popularmovies.MoviePostersActivity;
+import com.nanodegree.diego.popularmovies.adapter.MoviePostersListAdapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 
-public class LoadMoviesTask extends AsyncTask<Void, Void, ImageView[]> {
+public class LoadMoviesTask extends AsyncTask<String, Void, ImageView[]> {
 
     /**
      * Reference to the movie poster activity.
@@ -30,7 +43,20 @@ public class LoadMoviesTask extends AsyncTask<Void, Void, ImageView[]> {
     }
 
     @Override
-    protected ImageView[] doInBackground(Void... params) {
+    protected ImageView[] doInBackground(String... params) {
+        URL url;
+        String httpResponse;
+
+        if(!TextUtils.isEmpty(params[0])){
+            url = this.buildURL(params[0]);
+
+            if(url != null) {
+                httpResponse = this.getHttpResponse(url);
+
+
+            }
+        }
+
         return new ImageView[0];
     }
 
@@ -39,5 +65,65 @@ public class LoadMoviesTask extends AsyncTask<Void, Void, ImageView[]> {
         this.mParent.hideProgressBar();
 
         super.onPostExecute(imageViews);
+    }
+
+    /**
+     * Get the Http response from a specified url.
+     * @param url
+     *      The url where the http response will fetch from.
+     * @return
+     *      String containing the http response.
+     */
+    private String getHttpResponse(URL url){
+        HttpURLConnection httpConnection = null;
+        String httpResponse = null;
+        InputStream urlInputStream;
+
+        try {
+            // Opens http connection
+            httpConnection = (HttpURLConnection)url.openConnection();
+
+            // Gets the inputstream used to read from the urhttpl connection
+            urlInputStream = httpConnection.getInputStream();
+
+            // Use a scanner to retrieve the whole string
+            Scanner scanner = new Scanner(urlInputStream);
+            scanner.useDelimiter("\\A");
+
+            if(scanner.hasNext()){
+                httpResponse = scanner.next();
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }finally {
+            if(httpConnection != null){
+                httpConnection.disconnect();
+            }
+        }
+
+        return httpResponse;
+    }
+
+    /**
+     * Builds a URL upon a url string address.
+     * @param baseUrlString
+     *  url string address.
+     * @return
+     * Instance of URL class.
+     */
+    private URL buildURL(String baseUrlString){
+        Uri uri;
+        URL url = null;
+
+        uri = Uri.parse(baseUrlString).buildUpon().build();
+
+        try {
+            url = new URL(uri.toString());
+        }
+        catch (MalformedURLException ex){
+            ex.printStackTrace();
+        }
+
+        return url;
     }
 }
