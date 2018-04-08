5825dd91ead97a3e8ba9c97aa88ade35b9a1b74a
Diego Francisco <diegofrancisco.ks@gmail.com>
2018-04-08 16:45:08 -0300

    Correcting multiple onCreate() calls of MoviePosterActivity

--- PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/MoviePostersActivity.java
+++ PopularMovies/app/src/main/java/com/nanodegree/diego/popularmovies/MoviePostersActivity.java
@@ -1,7 +1,6 @@
 package com.nanodegree.diego.popularmovies;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -112,31 +111,6 @@ public class MoviePostersActivity extends AppCompatActivity implements MoviePost
 
         // Loads the data
         this.loadMovieData();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 
     /**
